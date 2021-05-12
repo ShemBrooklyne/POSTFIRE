394ac0405d170f4688a0b2e7e83c4592ef225d69
ShemBrooklyne <shemnyatti@gmail.com>
2021-05-12 16:44:14 +0300

    post fire pics. Thank you

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAaGI6E0PvRSnjZZluZaqGSf1d6R3WBuG4",
+    authDomain: "mygallery-b05d6.firebaseapp.com",
+    databaseURL: "https://mygallery-b05d6-default-rtdb.firebaseio.com",
+    projectId: "mygallery-b05d6",
+    storageBucket: "mygallery-b05d6.appspot.com",
+    messagingSenderId: "489613866937",
+    appId: "1:489613866937:web:dc4161d03469eaaf026868",
+    measurementId: "G-Z9J4T33N7W"
+  }
 };
